1de0c54e26d4cd04c252436f04e2816ab7e95e20
yakupselami4 <yakupselami4@gmail.com>
2022-02-17 17:14:35 +0300

    adding component who Rocket.cs in Rocket Ship. Defining A, D, Space.

---
+++ Assets/Rocket.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    void ProcessInput(){
+        if(Input.GetKey(KeyCode.Space)){
+            print("Thrusting");
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotating left");
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating right");
+        }
+        }
+}
