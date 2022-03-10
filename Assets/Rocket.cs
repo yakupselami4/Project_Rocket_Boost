50af8f266eb9b3a231f95e08bd748fbc8cc380c5
yakupselami4 <yakupselami4@gmail.com>
2022-03-10 11:27:39 +0300

    adding AudioSource in RocketShip

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -20,6 +22,12 @@ public class Rocket : MonoBehaviour
     void ProcessInput(){
         if(Input.GetKey(KeyCode.Space)){
             rigidBody.AddRelativeForce(Vector3.up);
+            if(!audioSource.isPlaying){
+                audioSource.Play();
+            }else{
+                audioSource.Stop();
+            }
+            
         }
         if (Input.GetKey(KeyCode.A))
         {
